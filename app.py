2db2fd416c3fb45d50b4272c074ca31e4f153a84
therushmorare <tshego.tt.morare@gmail.com>
2025-03-17 19:06:37 +0200

    updates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect,send_from_directory, jsonify,session, flash, send_file
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Sammy!'
+    return render_template('coming_soon.html')
+
+app.static_folder = 'static'
+if __name__ == "__main":
+    app.run(host='0.0.0.0',port = 5000, debug = False)#remove debug true when moving to production
