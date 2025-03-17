a86be9338cb8a88bd369a1f45dc8e644b10ab3ff
therushmorare <tshego.tt.morare@gmail.com>
2025-03-17 20:09:23 +0200

    timer fix

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect,send_from_directory, jsonify,session, flash, send_file
+import time
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('coming_soon.html')
+    end_time = int(time.mktime(time.strptime('19 Mar 2025 00:00:00', '%d %b %Y %H:%M:%S'))) * 1000  # Convert to milliseconds
+    return render_template('coming_soon.html', end_time=end_time)
 
 app.static_folder = 'static'
 if __name__ == "__main":
--- static/js/countdowntime.js
+++ static/js/countdowntime.js
@@ -1,92 +1,68 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $.fn.extend({ 
+  $.fn.extend({ 
 
-      countdown100: function(options) {
-        var defaults = {
-          timeZone: "",
-          endtimeYear: 0,
-          endtimeMonth: 0,
-          endtimeDate: 0,
-          endtimeHours: 0,
-          endtimeMinutes: 0,
-          endtimeSeconds: 0,
+    countdown100: function(options) {
+      var defaults = {
+        endtime: 0, // Expecting a timestamp in milliseconds
+      };
+
+      var options = $.extend(defaults, options);
+
+      return this.each(function() {
+        var obj = $(this);
+        
+        // Ensure `endtime` is a valid Date object
+        var deadline = new Date(parseInt(options.endtime)); 
+
+        if (isNaN(deadline.getTime())) {
+          console.error("Invalid endtime provided:", options.endtime);
+          return;
         }
 
-        var options =  $.extend(defaults, options);
+        initializeClock(deadline);
 
-        return this.each(function() {
-          var obj = $(this);
-          var timeNow = new Date();
+        function getTimeRemaining(endtime) { 
+          var now = new Date().getTime(); // Get current time in milliseconds
+          var t = endtime - now;
 
-          var tZ = options.timeZone; console.log(tZ);
-          var endYear = options.endtimeYear;
-          var endMonth = options.endtimeMonth;
-          var endDate = options.endtimeDate;
-          var endHours = options.endtimeHours;
-          var endMinutes = options.endtimeMinutes;
-          var endSeconds = options.endtimeSeconds;
-
-          if(tZ == "") {
-            var deadline = new Date(endYear, endMonth - 1, endDate, endHours, endMinutes, endSeconds);
-          } 
-          else {
-            var deadline = moment.tz([endYear, endMonth - 1, endDate, endHours, endMinutes, endSeconds], tZ).format();
+          if (t < 0) {
+            return { total: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
           }
 
-          if(Date.parse(deadline) < Date.parse(timeNow)) {
-            var deadline = new Date(Date.parse(new Date()) + endDate * 24 * 60 * 60 * 1000 + endHours * 60 * 60 * 1000); 
-          }
-          
-          
-          initializeClock(deadline);
+          var seconds = Math.floor((t / 1000) % 60);
+          var minutes = Math.floor((t / 1000 / 60) % 60);
+          var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+          var days = Math.floor(t / (1000 * 60 * 60 * 24));
 
-          function getTimeRemaining(endtime) { 
-            var t = Date.parse(endtime) - Date.parse(new Date());
-            var seconds = Math.floor((t / 1000) % 60);
-            var minutes = Math.floor((t / 1000 / 60) % 60);
-            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-            var days = Math.floor(t / (1000 * 60 * 60 * 24));
-            return {
-              'total': t,
-              'days': days,
-              'hours': hours,
-              'minutes': minutes,
-              'seconds': seconds
-            };
-          }
+          return { total: t, days: days, hours: hours, minutes: minutes, seconds: seconds };
+        }
 
-          function initializeClock(endtime) { 
-            var daysSpan = $(obj).find('.days');
-            var hoursSpan = $(obj).find('.hours');
-            var minutesSpan = $(obj).find('.minutes');
-            var secondsSpan = $(obj).find('.seconds');
+        function initializeClock(endtime) { 
+          var daysSpan = $(obj).find('.days');
+          var hoursSpan = $(obj).find('.hours');
+          var minutesSpan = $(obj).find('.minutes');
+          var secondsSpan = $(obj).find('.seconds');
 
-            function updateClock() { 
-              var t = getTimeRemaining(endtime);
+          function updateClock() { 
+            var t = getTimeRemaining(endtime);
 
-              daysSpan.html(t.days);
-              hoursSpan.html(('0' + t.hours).slice(-2));
-              minutesSpan.html(('0' + t.minutes).slice(-2));
-              secondsSpan.html(('0' + t.seconds).slice(-2))
+            daysSpan.html(t.days);
+            hoursSpan.html(('0' + t.hours).slice(-2));
+            minutesSpan.html(('0' + t.minutes).slice(-2));
+            secondsSpan.html(('0' + t.seconds).slice(-2));
 
-              if (t.total <= 0) {
-                clearInterval(timeinterval);
-              }
+            if (t.total <= 0) {
+              clearInterval(timeinterval);
             }
-
-            updateClock();
-            var timeinterval = setInterval(updateClock, 1000);
           }
 
-          
-
-
-        });
-      }
-    });
-
-    
+          updateClock();
+          var timeinterval = setInterval(updateClock, 1000);
+        }
+      });
+    }
+  });
 
 })(jQuery);
