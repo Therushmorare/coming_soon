5222ecd5938d3f874efc5fb0dbed7bed43f462fc
therushmorare <tshego.tt.morare@gmail.com>
2025-03-17 20:12:28 +0200

    timer fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    end_time = int(time.mktime(time.strptime('19 Mar 2025 00:00:00', '%d %b %Y %H:%M:%S'))) * 1000  # Convert to milliseconds
+    end_time = int(time.mktime(time.strptime('21 Mar 2025 00:00:00', '%d %b %Y %H:%M:%S'))) * 1000  # Convert to milliseconds
     return render_template('coming_soon.html', end_time=end_time)
 
 app.static_folder = 'static'
